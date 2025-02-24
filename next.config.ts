8357c455eb544046681734a0687420a15d05907e
Muhammed21 <mcavus048@gmail.com>
2025-02-24 20:15:54 +0100

    :zap: Modification du fichier next.config.ts pour contourner les erreurs CORS

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Autorise toutes les routes API
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Autorise toutes les origines
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" }, // Autorise ces méthodes
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
